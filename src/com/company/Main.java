586003bc4b06292c8e3c95431b258e2c8f212fd4
aminahaid <aminahaid@gmail.com>
2020-04-16 09:01:10 -0500

    f commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int workOutDays = 10;
+        double health = 8.5;
+        boolean powerUp = true;
+
+        System.out.println("Number of days: " + workOutDays + "\n" + "Health " +
+                "Scale: " + health + "\n" + "Are you ready to Power Up: " + powerUp + "\n" + "Total Assessment days: it has been days since you have started working out." + "\n"
+        +"You evaluate at "+ health + " out of " +  workOutDays + " on the health scale. "+"\n"+ "It is true that you are ready to power up your work out.");
+    }
+}
